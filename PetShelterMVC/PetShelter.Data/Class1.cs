97bd6ced1dcee7630cb5ca7a2da95d478c59f790
Petkov06 <k0889504247@gmail.com>
2024-03-19 09:17:27 +0200

    create project PetShelterMVC

---
+++ PetShelterMVC/PetShelter.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PetShelter.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ PetShelterMVC/PetShelter.Data/Data/PetShelterDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.Data.Data
+{
+    public class PetShelterDbContext : DbContext
+    {
+        public PetShelterDbContext(DbContextOptions<PetShelterDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
