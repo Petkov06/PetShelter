ec2d24fc5020058269fa2904b5b991f1610c33bf
Kristian Petkov <k0889504247.com>
2024-05-06 18:30:23 +0300

    add view models

---
+++ PetShelterMVC/PetShelter.Services/ViewModels/LoginVm.cs
@@ -0,0 +1,19 @@
+﻿using PetShelter.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.Services.ViewModels
+{
+    public class LoginVm : BaseVM
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public BreedSize Size { get; set; }
+    }
+}
