4727e606312b1f1d48ed1d0076033567ea4f80b0
Kristian Petkov <k0889504247.com>
2024-06-17 21:45:50 +0300

    redesigned

--- PetShelterMVC/PetShelter.Data/PetShelterDbContext.cs
+++ PetShelterMVC/PetShelter.Data/PetShelterDbContext.cs
@@ -1,13 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PetShelter.Data.Entities;
-using PetShelter.Shared;
 using PetShelter.Shared.Enums;
 using PetShelter.Shared.Security;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PetShelter.Data
 {
